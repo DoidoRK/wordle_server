29f1aa376c2c9fda62096517539071235cae1ae7
João Carlos <doidobr46@gmail.com>
2023-10-22 18:49:44 -0300

    Still not finding the word in the wordbank file.

--- src/utils/wordbank.h
+++ src/utils/wordbank.h
@@ -8,6 +8,8 @@
 #include <map>
 #include <pthread.h>
 #include <cstring> // Added for string manipulation
+#include <cctype>  // for toupper
+#include <algorithm>  // for transform
 
 #define WORDBANK_PATH "db/wordbank.txt"
 
@@ -16,23 +18,27 @@ using namespace std;
 pthread_mutex_t wordbank_db_mutex = PTHREAD_MUTEX_INITIALIZER;
 map<string, char[WORD_SIZE]> players_words; // Use 'string' for keys
 
-#include <cctype>  // for std::toupper
-#include <algorithm>  // for std::transform
-
-// Convert a string to uppercase
-std::string toUpperCase(const std::string& str) {
-    std::string result = str;
-    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
+string toUpperCase(const string& str) {
+    string result = str;
+    for (char& c : result) {
+        c = toupper(c);
+    }
     return result;
 }
 
-// Trim leading and trailing whitespace from a string
-std::string trim(const std::string& str) {
+string trim(const string& str) {
+    // Find the first non-whitespace character
     size_t first = str.find_first_not_of(" \t\n\r");
-    if (first == std::string::npos) {
-        return "";
+
+    if (string::npos == first) {
+        // No non-whitespace characters, return an empty string
+        return str;
     }
+
+    // Find the last non-whitespace character
     size_t last = str.find_last_not_of(" \t\n\r");
+
+    // Return the trimmed string
     return str.substr(first, (last - first + 1));
 }
 
@@ -41,28 +47,29 @@ bool searchWordInFile(const char word[WORD_SIZE]) {
     ifstream file(WORDBANK_PATH);
     if (!file.is_open()) {
         cerr << "Error: Unable to open the file." << endl;
-        pthread_mutex_unlock(&wordbank_db_mutex);
         return false;
     }
 
     char line[WORD_SIZE];
-    while (file.getline(line, WORD_SIZE)) {
-        // Trim any leading and trailing whitespace from the line
+
+    while (file.getline(line, WORD_SIZE)){
         string trimmedLine = line;
         trimmedLine = toUpperCase(trimmedLine); // Ensure case-insensitive comparison
         trimmedLine = trim(trimmedLine);
+        cout << "Searching inside file..." << trimmedLine << endl;
+        // Remove newline characters if they exist in the line
+        trimmedLine.erase(remove(trimmedLine.begin(), trimmedLine.end(), '\n'), trimmedLine.end());
 
         if (strcasecmp(trimmedLine.c_str(), word) == 0) {
             file.close();
             pthread_mutex_unlock(&wordbank_db_mutex);
-            cout << "ACHOU A PALAVRA!" << endl;
+            cout << "Word search finished, Word found: " << word << endl;
             return true;
         }
     }
-
     file.close();
     pthread_mutex_unlock(&wordbank_db_mutex);
-    cout << "NÃO ACHOU A PALAVRA!" << endl;
+    cout << "Word search finished, Word not found: " << word << endl;
     return false;
 }
 
